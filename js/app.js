932206e64d0e696017ce6db43086a83306c8e19b
DavidXploidz <david.adame.itd@gmail.com>
2024-09-27 13:48:29 -0600

    setup project

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Submenu
+let subMenu = document.querySelector(".menu__sub");
+
+window.addEventListener("click", (e) => {
+    if(e.target.closest(".toggle")){
+        subMenu.style.display = "flex";
+    }else{
+        subMenu.style.display = "none";
+    }
+});
